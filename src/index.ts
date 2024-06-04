5b6f285278d0369e69ebeb364cab209fa7aa77f1
vaidik2473 <vaidik2473@gmail.com>
2024-06-04 11:56:06 +0530

    c

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,85 @@
 import { Elysia } from "elysia";
 import { respinseData } from "../response";
+const port = process.env.PORT || 4000;
 
-const app = new Elysia().post("/hi", () => respinseData).listen(3000);
+const responseData = {
+  data: [
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+    respinseData,
+  ],
+};
+
+const responsev2 = {
+  data: [
+    responseData,
+    responseData,
+    responseData,
+    responseData,
+    responseData,
+    responseData,
+    responseData,
+    responseData,
+    responseData,
+    responseData,
+    responseData,
+    responseData,
+    responseData,
+    responseData,
+    responseData,
+    responseData,
+    responseData,
+    responseData,
+    responseData,
+    responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+    // responseData,
+  ],
+};
+
+const app = new Elysia()
+  .get("/", () => {
+    return {
+      respinseData,
+    };
+  })
+  .listen(port);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
